65d74b9c5afc2bd59e9e67ea449d2ac0dcccbd7a
megaton <megaton@github.com>
2016-03-02 12:45:54 -0800

    Parsing CSG to JSON

--- csg-convert/src/main.cpp
+++ csg-convert/src/main.cpp
@@ -8,73 +8,111 @@
 
 using namespace std;
 using namespace lars;
+using namespace json11;
 
-class CsgVisitor{
-  
-  double value;
-  unordered_map<string,double> variables;
+class CsgVisitor{  
   
 public:
-  
-  double get_value(){
-    return value;
+
+  Json getValue () {
+
+    return m_value;
   }
-  
-  double get_value(expression<CsgVisitor> e){
-    e.accept(this);
-    return get_value();
+
+  Json getValue (expression<CsgVisitor> e) {
+
+    e.accept (this);
+    return m_value;
   }
-  
-  void visit_number(expression<CsgVisitor> e){
-    value = stod(e.string());
+
+  std::pair<std::string, Json> getArgument (expression<CsgVisitor> e) {
+
+    return std::make_pair (e[0].string(), getValue (e[1]));
   }
-  
-  void visit_set_variable(expression<CsgVisitor> e){
-    variables[e[0].string()] = get_value(e[1]);
+
+  void visitNumber (expression<CsgVisitor> e) {
+
+    m_value = stod (e.string());
   }
-  
-  void visit_variable(expression<CsgVisitor> e){
-    value = variables[e[0].string()];
+
+  void visitBoolean (expression<CsgVisitor> e) {
+
+    m_value = e.string() == "true";
   }
-  
-  void visit_left_binary_operator_list (expression<CsgVisitor> e){
-    double lhs = get_value(e[0]);
-    
-    for(auto i:range((e.size()-1)/2)*2+1){
-      double rhs = get_value(e[i+1]);
-           if(e[i].character()=='+'){ lhs = lhs + rhs; }
-      else if(e[i].character()=='-'){ lhs = lhs - rhs; }
-      else if(e[i].character()=='*'){ lhs = lhs * rhs; }
-      else if(e[i].character()=='/'){ lhs = lhs / rhs; }
-      else throw "undefined operator";
+
+  void visitString (expression<CsgVisitor> e) {
+
+    m_value = e.string();
+  }
+
+  void visitArguments (expression<CsgVisitor> e) {
+
+    Json::object anArgs;
+
+    for (int i = 0; i < e.size(); ++i) {
+      auto anArg = getArgument (e[i]);
+      anArgs[anArg.first] = anArg.second;
     }
-    
-    value = lhs;
+
+    m_value = anArgs;
   }
-  
-  void visit_exponent(expression<CsgVisitor> e){
-    if(e.size() == 1) e[0].accept();
-    else value = pow(get_value(e[0]), get_value(e[1]));
+
+  void visitArray (expression<CsgVisitor> e) {
+
+    Json::array anArray;
+
+    for (int i = 0; i < e.size(); ++i) {
+      anArray.push_back (getValue (e[i]));
+    }
+
+    m_value = anArray;
   }
+ 
+  void visitObject (expression<CsgVisitor> e) {
+
+    Json::object anObject = Json::object({
+      { "type", e[0].string() },
+      { "properties", getValue (e[1]) },
+    });
+
+    m_value = anObject;
+  }
+
+  void visitInstruction (expression<CsgVisitor> e) {
+
+    Json::object anInstruction = Json::object({
+      { "type", e[0].string() },
+      { "properties", getValue (e[1]) },
+      { "objects", getValue (e[2]) },
+    });
+
+    m_value = anInstruction;
+  }
+
+private:
+
+  Json m_value;
   
 };
 
-int main(int argc, char ** argv){
+int main (int argc, char ** argv) {
   parsing_expression_grammar_builder<CsgVisitor> aGrammar;
-  using Expression = expression<CsgVisitor>;
+  using expression = expression<CsgVisitor>;
 
-  auto propagate = [](Expression e){ for(auto c:e)c.accept(); };
+  auto propagate = [](expression e){ for (auto c : e) c.accept(); };
   
-  aGrammar["ObjectList"]   << "( Comment | Object | Instruction )+" << propagate;
-  aGrammar["Comment"]      << "'#' String" << [ ](Expression e){ std::cout << "Comment: " << e[0].string() << std::endl; };
-  aGrammar["Instruction"]  << "Name '(' ArgumentList ')' ' '* '{' ObjectList '}'" << propagate;
-  aGrammar["Object"  ]     << "Name '(' ArgumentList ')' ';'" << propagate;
-  aGrammar["ArgumentList"] << " Argument ( ',' Argument )* | Argument? " << propagate;
-  aGrammar["Argument"]     << "Name '=' ( Number | '\"' String '\"' | Boolean )"     << [ ](Expression e){ std::cout << e[0].string() << " | " << e[1].string() << std::endl; };
-  aGrammar["Number"  ]     << "'-'? [0-9]+ ('.' [0-9]+)?"               ;
-  aGrammar["Boolean" ]     << "'true' | 'false'"               << [ ](Expression e){};
-  aGrammar["Name"    ]     << "[a-zA-Z] [a-zA-Z]*" << [ ](Expression e){ std::cout << "Name found: " << std::endl; };
-  aGrammar["String"  ]     << "[0-9a-zA-Z ]*" << [ ](Expression e){ std::cout << "String found: "  << std::endl; };
+  aGrammar["ObjectList"]   << "( Comment | Object | Instruction )+"               << [](expression e){ e.visitor().visitArray (e); };
+  aGrammar["Comment"]      << "'#' String"                                        << [](expression e){ std::cout << "Comment: " << e[0].string() << std::endl; };
+  aGrammar["Instruction"]  << "Name '(' ArgumentList ')' ' '* '{' ObjectList '}'" << [](expression e){ e.visitor().visitInstruction (e); };
+  aGrammar["Object"  ]     << "Name '(' ArgumentList ')' ';'"                     << [](expression e){ e.visitor().visitObject (e); };
+  aGrammar["ArgumentList"] << " Argument ( ',' Argument )* | Argument? "          << [](expression e){ e.visitor().visitArguments (e); };
+  aGrammar["Argument"]     << "Name '=' ( Value )"                                ;
+  aGrammar["Array"]        << "'[' ( Value ( ',' Value )* | (Value)? ) ']'"       << [](expression e){ e.visitor().visitArray (e); };
+  aGrammar["Value"   ]     << "Number | '\"' String '\"' | Boolean | Array"       << propagate;
+  aGrammar["Number"  ]     << "'-'? [0-9]+ ('.' [0-9]+)?"                         << [](expression e){ e.visitor().visitNumber (e); };
+  aGrammar["Boolean" ]     << "'true' | 'false'"                                  << [](expression e){ e.visitor().visitBoolean (e); };
+  aGrammar["Name"    ]     << "[a-zA-Z] [a-zA-Z]*"                                ;
+  aGrammar["String"  ]     << "[0-9a-zA-Z ]*"                                     << [](expression e){ e.visitor().visitString (e); };
   
   aGrammar.set_starting_rule("ObjectList");
   
@@ -82,7 +120,7 @@ int main(int argc, char ** argv){
   
   aGrammar.set_separator_rule("Whitespace");
   
-  auto p = aGrammar.get_parser();
+  auto aParser = aGrammar.get_parser();
   
   CsgVisitor visitor;
 
@@ -90,14 +128,18 @@ int main(int argc, char ** argv){
 
   aTestStrs.push_back("#version csg-core 0.3");
   aTestStrs.push_back("box();");
-  // aTestStrs.push_back("");
   aTestStrs.push_back("sphere(r=5.0);");
-  aTestStrs.push_back("sphere(r=true);");
+  aTestStrs.push_back("matrix(m=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);");
+  aTestStrs.push_back("sphere (r=true);");
   aTestStrs.push_back("sphere(r=\"three\");");
   aTestStrs.push_back("sphere(r=5.0, r=\"asd\");");
   aTestStrs.push_back("sphere(r=5.0, r=\"asd\", topYa=\"4\");");
   aTestStrs.push_back("union(r=5.0, r=\"asd\") { sphere(); }");
   aTestStrs.push_back("union(r=5.0, r=\"asd\") { sphere(); box(); }");
+
+  aTestStrs.push_back("sphere (r=true);\n"
+                      "sphere(r=\"three\");\n"
+                      "sphere(r=5.0, r=\"asd\");");
 
   while (true) {
     string str;
@@ -113,9 +155,8 @@ int main(int argc, char ** argv){
     }
     if(str == "q" || str == "quit")break;
     try { 
-      p.parse(str).accept(&visitor); 
-      // cout << str << " = " << visitor.get_value() << endl;
-      cout << "Correct" << endl;
+      aParser.parse (str).accept (&visitor); 
+      cout << Json (visitor.getValue()).dump() << endl;
     }
     catch (parser<CsgVisitor>::error e){
       cout << "  ";
@@ -126,13 +167,5 @@ int main(int argc, char ** argv){
     }
   }
 
-  json11::Json obj = json11::Json::object({
-      { "k1", "v1" },
-      { "k2", 42.0 },
-      { "k3", json11::Json::array({ "a", 123.0, true, false, nullptr }) },
-  });
-
-  std::cout << "obj: " << obj.dump() << "\n";
-  
   return 0;
 }
